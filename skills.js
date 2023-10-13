3a2a677808896e9eaa90c34f145973eb3f0d1018
Ceeks <sikaantwi@gmail.com>
2023-10-13 11:50:43 +0000

    created a js file for skills

---
+++ skills.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+    // Show the button when scrolling down
+    window.addEventListener('scroll', function() {
+      if (window.pageYOffset > 100) {
+        scrollToTopBtn.style.display = 'block';
+      } else {
+        scrollToTopBtn.style.display = 'none';
+      }
+    });
+  
+    // Scroll to top when the button is clicked
+    scrollToTopBtn.addEventListener('click', function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
