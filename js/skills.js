fb0c514c6cac40f3595387b30017b4c6fdce8296
imKaba <mahamadou.kaba@meltwater.org>
2023-10-20 03:28:48 +0000

    responsiveness reviewed

--- js/skills.js
+++ js/skills.js
@@ -1,22 +1,44 @@
-// Put this updated JavaScript code in your existing `script.js` file
+const textElement = document.getElementById('text');
+const texts = [
+    "Business Development",
+    "Research and Admin",
+    "Presentation Design",
+    "UX/UI Design",
+    "Photography"
+];
+let textIndex = 0;
+let charIndex = 0;
 
-document.addEventListener('DOMContentLoaded', function() {
-    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
-  
-    // Show the button when scrolling down
-    window.addEventListener('scroll', function() {
-      if (window.pageYOffset > 100) {
-        scrollToTopBtn.style.display = 'block';
-      } else {
-        scrollToTopBtn.style.display = 'none';
-      }
-    });
-  
-    // Scroll to top when the button is clicked
-    scrollToTopBtn.addEventListener('click', function() {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-    });
-  });
+function typeText() {
+    if (charIndex < texts[textIndex].length) {
+        textElement.textContent += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, 50); // Adjust the typing speed here
+    } else {
+        setTimeout(eraseText, 2000); // Wait before erasing the text
+    }
+}
+
+function eraseText() {
+    if (charIndex > 0) {
+        textElement.textContent = texts[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseText, 30); // Adjust the erasing speed here
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        setTimeout(typeText, 1000); // Wait before typing the next text
+    }
+}
+
+// Start the animation
+typeText();
+
+
+
+                
+const burger = document.getElementById("burger");
+const menu = document.querySelector("ul.navbar");
+
+burger.addEventListener("click", function () {
+    menu.classList.toggle("menu-opened");
+});
